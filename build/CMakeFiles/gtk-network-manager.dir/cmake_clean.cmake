da1de65e8d4ca3627f4620ec3a9b20bdadb19f95
Ashwin Prabhakar <ashwin.prabhakar@oss.qualcomm.com>
2026-07-09 14:00:05 +0200

    Update README.md
    Add a README.md file as a placeholder to update project details

---
+++ build/CMakeFiles/gtk-network-manager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtk-network-manager.dir/src/main.c.o"
+  "CMakeFiles/gtk-network-manager.dir/src/main.c.o.d"
+  "gtk-network-manager"
+  "gtk-network-manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gtk-network-manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
